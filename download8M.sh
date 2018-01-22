905dff750b1a5428cd841f4c1e66b9fd03818540
Anshuman Suri <anshuman14021@iiitd.ac.in>
2018-01-23 00:45:08 +0530

    Added (untested) data parsing code, partially taken from https://github.com/google/youtube-8m

--- download8M.sh
+++ download8M.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-mkdir -p ~/data/yt8m_video_level; cd ~/data/yt8m_video_level
+mkdir -p data/yt8m_video_level; cd data/yt8m_video_level
 
-for (( i=1; i<=4096; i++))
-do
-	curl data.yt8m.org/download.py | partition=1/video_level/train mirror=asia python
-	curl data.yt8m.org/download.py | partition=1/video_level/validate mirror=asia python
-	curl data.yt8m.org/download.py | partition=1/video_level/test mirror=asia python
-done
+
+curl data.yt8m.org/download.py | partition=1/video_level/train mirror=asia python
+curl data.yt8m.org/download.py | partition=1/video_level/validate mirror=asia python
+curl data.yt8m.org/download.py | partition=1/video_level/test mirror=asia python
