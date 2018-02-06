e68eec4aea6653324175a3e029d6a0c25c09d042
Anshuman Suri <anshuman14021@iiitd.ac.in>
2018-02-06 23:51:47 +0530

    Adding code for downloading and caching metadata

--- code/simple_reader.py
+++ code/simple_reader.py
@@ -1,14 +1,13 @@
 import json
 import numpy as np
 import os
+import base64
 import tensorflow as tf
 from google.protobuf.json_format import MessageToJson
 
-LOC = '/home/anshumans/Desktop/Studies/IR/IR_project/data/dummydata'
 
-
-def extract_relevant_info(json_parsed_record):
-	video_id = json_parsed_record['features']['feature']['video_id']['bytesList']['value'][0]
+def extract_relevant_info(json_parsed_record, which_generator):
+	video_id = base64.b64decode(json_parsed_record['features']['feature']['video_id']['bytesList']['value'][0])
 	labels = json_parsed_record['features']['feature']['labels']['int64List']['value']
 	mean_audio = json_parsed_record['features']['feature']['mean_audio']['floatList']['value']
 	mean_rgb = json_parsed_record['features']['feature']['mean_rgb']['floatList']['value']
@@ -17,28 +16,29 @@ def extract_relevant_info(json_parsed_record):
 		,"labels": labels
 		,"mean_audio": mean_audio
 		,"mean_rgb": mean_rgb
+		,"which_generator": which_generator
 	}
 	return parsed_record
 
 
-def generator(which_generator, batch_size):
-	global LOC
-	protobuf_records = filter(lambda x: 'tfrecord' in x, os.listdir(LOC))
+def generator(location, which_generator, batch_size):
+	protobuf_records = filter(lambda x: 'tfrecord' in x, os.listdir(location))
 	if (which_generator == 1):
 		protobuf_records = filter(lambda x: 'train' in x, protobuf_records)
 	elif (which_generator == 2):
 		protobuf_records = filter(lambda x: 'test' in x, protobuf_records)
 	elif (which_generator == 3):
 		protobuf_records = filter(lambda x: 'validate' in x, protobuf_records)
-	protobuf_records = map(lambda x: os.path.join(LOC, x), protobuf_records)
+	protobuf_records = map(lambda x: os.path.join(location, x), protobuf_records)
 	i = 0
-	while True:
+	for j in range(len(protobuf_records) // batch_size):
+		print j, "out of", len(protobuf_records) // batch_size, "read"
 		i %= batch_size
 		json_parsed_records = []
 		for record in protobuf_records[i * batch_size : (i + 1) * batch_size]:
 			for record_ in tf.python_io.tf_record_iterator(record):
 				json_parsed_record = json.loads(MessageToJson(tf.train.Example.FromString(record_)))
-				json_parsed_records.append(extract_relevant_info(json_parsed_record))
+				json_parsed_records.append(extract_relevant_info(json_parsed_record, which_generator))
 		i += 1
 		if (len(json_parsed_records) == 0):
 			continue
@@ -46,6 +46,7 @@ def generator(which_generator, batch_size):
 
 
 if __name__ == "__main__":
-	gen = generator(1, 1)
+	location = '/home/anshumans/Desktop/Studies/IR/IR_project/data/dummydata'
+	gen = generator(location, 1, 1)
 	arr = gen.next()
 	print arr[0]
