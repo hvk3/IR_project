def2df89df3ac3d4df6e0119951669ef1db06604
Anshuman Suri <anshuman14021@iiitd.ac.in>
2018-03-18 00:02:37 +0530

    Prior to refactoring commit

--- code/simple_reader.py
+++ code/simple_reader.py
@@ -2,177 +2,192 @@ import json
 import numpy as np
 import os
 import base64
+import pickle
 import tensorflow as tf
-from keras.preprocessing.text import text_to_word_sequence, one_hot, Tokenizer
+from keras.preprocessing.text import Tokenizer
 from google.protobuf.json_format import MessageToJson
 from gensim.models import doc2vec
-
 
 # For preparing text for LSTM model, use this
 # https://machinelearningmastery.com/prepare-text-data-deep-learning-keras/
 
-def get_titles(collection):
-	titles = []
-	vocab = {}
-	count = 0
-	for record in collection.find({}):
-		title = record['metadata']['metadata']['title']
-		for word in title.split(' '):
-			word = word.lower().strip()
-			if (word not in vocab):
-				vocab[word] = count
-				count += 1
-		titles.append(map(lambda x: vocab[x.lower().strip()], title.split(' ')))
-	np.save('titles.npy', titles)
-	np.save('vocab.npy', vocab)
-	return titles
+
+def title_generator(collection, batch_size):
+    i = 0
+    titles = []
+    for record in collection.find({}):
+        title = record['metadata']['metadata']['title'].encode('utf-8')
+        titles.append(title)
+    try:
+        with open('tokenizer.pickle', 'rb') as handle:
+            t = pickle.load(handle)
+    except IOError:
+        t = Tokenizer()
+        t.fit_on_texts(titles)
+        max_len = max(map(lambda x: len(x.split(' ')), titles))
+        np.save('max_seq_length.npy', max_len)
+        with open('tokenizer.pickle', 'wb') as handle:
+            pickle.dump(t, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print batch_size
+    while (True):
+        i %= len(titles) // batch_size
+        print len(titles[i * batch_size: min(len(titles), (i + 1) * batch_size)])
+        encoded_titles = t.texts_to_matrix(titles[i * batch_size: min(len(titles), (i + 1) * batch_size)])
+        i += 1
+        yield encoded_titles
 
 
 def extract_relevant_info(json_parsed_record, which_generator):
-	video_id = base64.b64decode(json_parsed_record['features']['feature']['video_id']['bytesList']['value'][0])
-	try:
-		labels = json_parsed_record['features']['feature']['labels']['int64List']['value']
-	except:
-		labels = []
-	mean_audio = json_parsed_record['features']['feature']['mean_audio']['floatList']['value']
-	mean_rgb = json_parsed_record['features']['feature']['mean_rgb']['floatList']['value']
-	parsed_record = {
-		"video_id": video_id
-		,"labels": labels
-		,"mean_audio": mean_audio
-		,"mean_rgb": mean_rgb
-		,"which_generator": which_generator
-	}
-	return parsed_record
+    video_id = base64.b64decode(json_parsed_record['features']['feature']['video_id']['bytesList']['value'][0])
+    try:
+        labels = json_parsed_record['features']['feature']['labels']['int64List']['value']
+    except KeyError:
+        labels = []
+    mean_audio = json_parsed_record['features']['feature']['mean_audio']['floatList']['value']
+    mean_rgb = json_parsed_record['features']['feature']['mean_rgb']['floatList']['value']
+    parsed_record = {
+        "video_id": video_id,
+        "labels": labels,
+        "mean_audio": mean_audio,
+        "mean_rgb": mean_rgb,
+        "which_generator": which_generator
+    }
+    return parsed_record
 
 
 def generator(location, which_generator, batch_size):
-	protobuf_records = filter(lambda x: 'tfrecord' in x, os.listdir(location))
-	if (which_generator == 1):
-		protobuf_records = filter(lambda x: 'train' in x, protobuf_records)
-	elif (which_generator == 2):
-		protobuf_records = filter(lambda x: 'test' in x, protobuf_records)
-	elif (which_generator == 3):
-		protobuf_records = filter(lambda x: 'validate' in x, protobuf_records)
-	protobuf_records = map(lambda x: os.path.join(location, x), protobuf_records)
-	i = 0
-	for j in range(len(protobuf_records) // batch_size):
-		print(j, "out of", len(protobuf_records) // batch_size, "done")
-		i %= batch_size
-		json_parsed_records = []
-		for record in protobuf_records[i * batch_size : (i + 1) * batch_size]:
-			for record_ in tf.python_io.tf_record_iterator(record):
-				json_parsed_record = json.loads(MessageToJson(tf.train.Example.FromString(record_)))
-				json_parsed_records.append(extract_relevant_info(json_parsed_record, which_generator))
-		i += 1
-		if (len(json_parsed_records) == 0):
-			continue
-		yield np.array(json_parsed_records)
+    protobuf_records = filter(lambda x: 'tfrecord' in x, os.listdir(location))
+    if (which_generator == 1):
+        protobuf_records = filter(lambda x: 'train' in x, protobuf_records)
+    elif (which_generator == 2):
+        protobuf_records = filter(lambda x: 'test' in x, protobuf_records)
+    elif (which_generator == 3):
+        protobuf_records = filter(lambda x: 'validate' in x, protobuf_records)
+    protobuf_records = map(lambda x: os.path.join(location, x), protobuf_records)
+    i = 0
+    for j in range(len(protobuf_records) // batch_size):
+        print(j, "out of", len(protobuf_records) // batch_size, "done")
+        i %= batch_size
+        json_parsed_records = []
+        for record in protobuf_records[i * batch_size : (i + 1) * batch_size]:
+            for record_ in tf.python_io.tf_record_iterator(record):
+                json_parsed_record = json.loads(MessageToJson(tf.train.Example.FromString(record_)))
+                json_parsed_records.append(extract_relevant_info(json_parsed_record, which_generator))
+        i += 1
+        if (len(json_parsed_records) == 0):
+            continue
+        yield np.array(json_parsed_records)
 
 
 def rankAndSelectComments(comments, select):
-	likes = []
-	for comment in comments:
-		likes.append(int(comment['likes']))
-	sortedByLikes = np.argsort(likes)[::-1]
-	selectedComments = []
-	for i in range(len(sortedByLikes)):
-		selectedComments.append(comments[sortedByLikes[i]]['comment'])
-	return selectedComments[:select]
+    likes = []
+    for comment in comments:
+        likes.append(int(comment['likes']))
+    sortedByLikes = np.argsort(likes)[::-1]
+    selectedComments = []
+    for i in range(len(sortedByLikes)):
+        selectedComments.append(comments[sortedByLikes[i]]['comment'])
+    return selectedComments[:select]
 
 
 def filled_metadata(metadata):
-	commentCount =  metadata.get('commentCount', 0)
-	viewCount = metadata.get('viewCount', 0)
-	favoriteCount =  metadata.get('favoriteCount', 0)
-	dislikeCount = metadata.get('dislikeCount', 0)
-	likeCount = metadata.get('likeCount', 0)
-	return [commentCount, viewCount, favoriteCount, dislikeCount, likeCount]
+    commentCount =  metadata.get('commentCount', 0)
+    viewCount = metadata.get('viewCount', 0)
+    favoriteCount =  metadata.get('favoriteCount', 0)
+    dislikeCount = metadata.get('dislikeCount', 0)
+    likeCount = metadata.get('likeCount', 0)
+    return [commentCount, viewCount, favoriteCount, dislikeCount, likeCount]
 
 
 def mongoDBgenerator(collection, d2vmodel, numComments, which, batch_size, validation_ratio, use_audio, use_video
-		, use_desc, use_metadata, use_comments, use_channel):
-	print("Found", collection.find({"which":which}).count(), "records")
-	while True:
-		i = 0
-		X_metadata = []
-		X_audio = []
-		X_video = []
-		X_desc = []
-		X_comments = [ [] for _ in range(numComments)]
-		X_channel = []
-		Y = []
-		for record in collection.find({'which': which}):
-			metadata = record['metadata']['metadata']['statistics']
-			if len(record['metadata']['metadata']['comments']) < numComments:
-				continue
-			metadata_records = filled_metadata(metadata) + [record['metadata']['metadata']['categoryId']]
-			metadata_records = np.array([int(x) for x in metadata_records])
-			X_metadata.append(metadata_records)
-			# Add metadata to training data point
-			audio_data = record['avdata']['mean_audio']
-			X_audio.append(audio_data)
-			# Add audio features
-			video_data = record['avdata']['mean_rgb']
-			X_video.append(video_data)
-			# Add video features
-			description = d2vmodel.infer_vector(record['metadata']['metadata']['description'].split(' '))
-			X_desc.append(description)
-			# Add description features
-			selectedComments = rankAndSelectComments(record['metadata']['metadata']['comments'], numComments)
-			for i in range(numComments):
-				X_comments[i].append(d2vmodel.infer_vector(selectedComments[i].split(' ')))
-			# Add comment based features
-			channelTitle = d2vmodel.infer_vector(record['metadata']['metadata']['channelTitle'].split(' '))
-			X_channel.append(channelTitle)
-			# Add channel title features
-			Y.append(d2vmodel.infer_vector(record['metadata']['metadata']['title'].split(' ')))
-			if len(Y) == batch_size:
-				X = []
-				if (use_metadata):
-					X.append(np.array(X_metadata))
-				if (use_audio):
-					X.append(np.array(X_audio))
-				if (use_video):
-					X.append(np.array(X_video))
-				if (use_desc):
-					X.append(np.array(X_desc))
-				if (use_comments):
-					X += [ np.array(commentVec) for commentVec in X_comments ]
-				if (use_channel):
-					X.append(np.array(X_channel))
-				Y = np.array(Y)
-				# Split into train and validation if training
-				if which == 1:
-					splitPoint = int(validation_ratio * len(Y))
-					X_train = [ x[splitPoint:] for x in X]
-					X_val = [ x[:splitPoint] for x in X]
-					yield (X_train, Y[splitPoint:]), (X_val, Y[:splitPoint])
-				else:
-					yield X, Y
-				X_metadata, X_audio, X_video, X_desc, X_channel = [], [], [], [], []
-				Y, X_comments = [], [ [] for _ in range(numComments) ]
-		# Don't loop indefinitely if test data
-		if which != 1:
-			break
+        , use_desc, use_metadata, use_comments, use_channel, use_titles=False):
+    print("Found", collection.find({"which":which}).count(), "records")
+    print batch_size
+    if (use_titles):
+        title_gen = title_generator(collection, batch_size)
+    while True:
+        i = 0
+        X_metadata = []
+        X_audio = []
+        X_video = []
+        X_desc = []
+        X_comments = [ [] for _ in range(numComments)]
+        X_channel = []
+        Y = []
+        for record in collection.find({'which': which}):
+            metadata = record['metadata']['metadata']['statistics']
+            if len(record['metadata']['metadata']['comments']) < numComments:
+                continue
+            metadata_records = filled_metadata(metadata) + [record['metadata']['metadata']['categoryId']]
+            metadata_records = np.array([int(x) for x in metadata_records])
+            X_metadata.append(metadata_records)
+            # Add metadata to training data point
+            audio_data = record['avdata']['mean_audio']
+            X_audio.append(audio_data)
+            # Add audio features
+            video_data = record['avdata']['mean_rgb']
+            X_video.append(video_data)
+            # Add video features
+            description = d2vmodel.infer_vector(record['metadata']['metadata']['description'].split(' '))
+            X_desc.append(description)
+            # Add description features
+            selectedComments = rankAndSelectComments(record['metadata']['metadata']['comments'], numComments)
+            for i in range(numComments):
+                X_comments[i].append(d2vmodel.infer_vector(selectedComments[i].split(' ')))
+            # Add comment based features
+            channelTitle = d2vmodel.infer_vector(record['metadata']['metadata']['channelTitle'].split(' '))
+            X_channel.append(channelTitle)
+            # Add channel title features
+            if (not use_titles):
+                Y.append(d2vmodel.infer_vector(record['metadata']['metadata']['title'].split(' ')))
+            else:
+                Y = title_gen.next()
+            if len(Y) == batch_size:
+                X = []
+                if (use_metadata):
+                    X.append(np.array(X_metadata))
+                if (use_audio):
+                    X.append(np.array(X_audio))
+                if (use_video):
+                    X.append(np.array(X_video))
+                if (use_desc):
+                    X.append(np.array(X_desc))
+                if (use_comments):
+                    X += [ np.array(commentVec) for commentVec in X_comments ]
+                if (use_channel):
+                    X.append(np.array(X_channel))
+                Y = np.array(Y)
+                # Split into train and validation if training
+                if which == 1:
+                    splitPoint = int(validation_ratio * len(Y))
+                    X_train = [ x[splitPoint:] for x in X]
+                    X_val = [ x[:splitPoint] for x in X]
+                    yield (X_train, Y[splitPoint:]), (X_val, Y[:splitPoint])
+                else:
+                    yield X, Y
+                X_metadata, X_audio, X_video, X_desc, X_channel = [], [], [], [], []
+                Y, X_comments = [], [ [] for _ in range(numComments) ]
+        # Don't loop indefinitely if test data
+        if which != 1:
+            break
 
 
 def getTrainValGens(sourceGen, train=True):
-	if train:
-		for tuple in sourceGen:
-			yield tuple[0]
-	else:
-		for tuple in sourceGen:
-			yield tuple[1]
+    if train:
+        for tuple in sourceGen:
+            yield tuple[0]
+    else:
+        for tuple in sourceGen:
+            yield tuple[1]
 
 
 if __name__ == "__main__":
-	from pymongo import MongoClient
-	client = MongoClient()
-	db = client.youtube8m
-	ds = db.iteration2
-	d2v = doc2vec.Doc2Vec.load('doc2vec_100.model')
-	gen = mongoDBgenerator(ds, d2v, 2, 1, 16) 
-	x, y = gen.next()
-	print x.shape, y.shape
+    from pymongo import MongoClient
+    client = MongoClient()
+    db = client.youtube8m
+    ds = db.iteration2
+    d2v = doc2vec.Doc2Vec.load('doc2vec_100.model')
+    gen = mongoDBgenerator(ds, d2v, 2, 1, 16)
+    x, y = gen.next()
+    print x.shape, y.shape
 
--- code/train_e2e.py
+++ code/train_e2e.py
@@ -1,11 +1,13 @@
 import models
+import numpy as np
+import os
 import simple_reader
+import pickle
 from pymongo import MongoClient
 from gensim.models import doc2vec
 from itertools import tee
 from keras.utils import plot_model
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau
-import numpy as np
 
 import tensorflow as tf
 from tensorflow.python.platform import flags
@@ -13,13 +15,13 @@ import keras
 
 # Dynamic memory growth
 config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
+config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 keras.backend.set_session(sess)
 
 # Define flags
 FLAGS = flags.FLAGS
-flags.DEFINE_integer('numComments', 3, 'Number of comments ot be consideredwhile building and training model')
+flags.DEFINE_integer('numComments', 3, 'Number of comments to be considered while building and training model')
 flags.DEFINE_integer('batchSize', 16, 'Batch size')
 flags.DEFINE_integer('numEpochs', 50, 'Number of epochs')
 flags.DEFINE_boolean('add_batch_norm', False, 'Add batch normalization to model?')
@@ -33,61 +35,85 @@ flags.DEFINE_boolean('vec2doc', False, 'Train on text, instead of doc2vec featur
 
 
 if __name__ == "__main__":
-	embeddingSize = 400
-	# Define model
-	if FLAGS.no_metadata:
-		model = models.no_metadata_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	elif FLAGS.no_audio:
-		model = models.no_audio_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	elif FLAGS.no_video:
-		model = models.no_video_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	elif FLAGS.no_description:
-		model = models.no_description_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	elif FLAGS.no_channel:
-		model = models.no_channelname_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	elif FLAGS.numComments == 0:
-		model = models.no_comments_model(embeddingSize, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	elif FLAGS.vec2doc:
-		# print("Sorry, not implemented yet!")
-		model = models.sent2vec_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	else:
-		model = models.no_sent2vec_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
-	# Visualize model
-	plot_model(model, to_file='model_arch.png')
-	client = MongoClient()
-	database = client.youtube8m
-	collection = database.iteration3
-	d2v = doc2vec.Doc2Vec.load('doc2vec_100.model')
-	# Load data generator
-	dataGen =  simple_reader.mongoDBgenerator(collection, d2v, FLAGS.numComments,1, FLAGS.batchSize, 0.2,
-                        use_audio=(FLAGS.no_audio==False),
-                        use_video=(FLAGS.no_video==False),
-                        use_desc=(FLAGS.no_description==False),
-                        use_metadata=(FLAGS.no_metadata==False),
-                        use_comments = (FLAGS.numComments > 0),
-                        use_channel = (FLAGS.no_channel==False))
-	# Split into train and validation generators
-	dataGen1, dataGen2 = tee(dataGen)
-	trainGen, valGen = simple_reader.getTrainValGens(dataGen1, True), simple_reader.getTrainValGens(dataGen2, False)	
-	# Load test generator
-	testGen = simple_reader.mongoDBgenerator(collection, d2v, FLAGS.numComments, 3, FLAGS.batchSize, 0.2,
-			use_audio=(FLAGS.no_audio==False),
-			use_video=(FLAGS.no_video==False),
-			use_desc=(FLAGS.no_description==False),
-			use_metadata=(FLAGS.no_metadata==False),
-			use_comments = (FLAGS.numComments > 0),
-			use_channel = (FLAGS.no_channel==False))
-	# Train model
-	early_stop = EarlyStopping(monitor='val_loss', min_delta=0.1, patience=5, verbose=1)
-	reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, min_lr=0.01, verbose=1)
-	import pdb;pdb.set_trace()
-	model.fit_generator(trainGen, validation_data=valGen, validation_steps=200, steps_per_epoch=1000, epochs=FLAGS.numEpochs, callbacks=[early_stop, reduce_lr])
-	# Ealuate model on test data
-	finalLoss = 0.0
-	for testBatch in testGen:
-		if testBatch:
-			finalLoss += model.evaluate(testBatch[0], testBatch[1], verbose=0) / len(testBatch[1])
-		else:
-			break
-	print("Loss on test data:", finalLoss)
+    embeddingSize = 400
+    # Define model
+    client = MongoClient()
+    database = client.youtube8m
+    collection = database.iteration3
+    titles = []
+    for record in collection.find({}):
+        title = record['metadata']['metadata']['title'].encode('utf-8')
+        titles.append(title)
+    import pdb;pdb.set_trace()
+    if FLAGS.no_metadata:
+        model = models.no_metadata_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    elif FLAGS.no_audio:
+        model = models.no_audio_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    elif FLAGS.no_video:
+        model = models.no_video_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    elif FLAGS.no_description:
+        model = models.no_description_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    elif FLAGS.no_channel:
+        model = models.no_channelname_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    elif FLAGS.numComments == 0:
+        model = models.no_comments_model(embeddingSize, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    elif FLAGS.vec2doc:
+        # print("Sorry, not implemented yet!")
+        if not (os.path.exists('tokenizer.pickle')):
+            gen = simple_reader.title_generator(collection, FLAGS.batchSize)
+            gen.next()
+        tokenizer = pickle.load(open('tokenizer.pickle', 'r'))
+        max_len = np.load('max_seq_length.npy').item()
+        model = models.sent2vec_model(embeddingSize, FLAGS.numComments, len(tokenizer.word_counts), max_len, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    else:
+        model = models.no_sent2vec_model(embeddingSize, FLAGS.numComments, FLAGS.add_batch_norm, FLAGS.add_dropout)
+    # Visualize model
+    plot_model(model, to_file='model_arch.png')
+    d2v = doc2vec.Doc2Vec.load('doc2vec_100.model')
+    # Load data generator
+    dataGen = simple_reader.mongoDBgenerator(
+        collection,
+        d2v,
+        FLAGS.numComments,
+        1,
+        FLAGS.batchSize,
+        0.2,
+        use_audio=not FLAGS.no_audio,
+        use_video=not FLAGS.no_video,
+        use_desc=not FLAGS.no_description,
+        use_metadata=not FLAGS.no_metadata,
+        use_comments=(FLAGS.numComments > 0),
+        use_channel=not FLAGS.no_channel,
+        use_titles=FLAGS.vec2doc)
+    # Split into train and validation generators
+    dataGen1, dataGen2 = tee(dataGen)
+    trainGen, valGen = simple_reader.getTrainValGens(dataGen1, True), simple_reader.getTrainValGens(dataGen2, False)
+    # Load test generator
+    testGen = simple_reader.mongoDBgenerator(
+        collection,
+        d2v,
+        FLAGS.numComments,
+        3,
+        FLAGS.batchSize,
+        0.2,
+        use_audio=not FLAGS.no_audio,
+        use_video=not FLAGS.no_video,
+        use_desc=not FLAGS.no_description,
+        use_metadata=not FLAGS.no_metadata,
+        use_comments=(FLAGS.numComments > 0),
+        use_channel=not FLAGS.no_channel,
+        use_titles=FLAGS.vec2doc)
+    # Train model
+    early_stop = EarlyStopping(monitor='val_loss', min_delta=0.1, patience=5, verbose=1)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, min_lr=0.01, verbose=1)
+    import pdb;pdb.set_trace()
+    model.fit_generator(trainGen, validation_data=valGen, validation_steps=200, steps_per_epoch=1000, epochs=FLAGS.numEpochs, callbacks=[early_stop, reduce_lr])
+    # Evaluate model on test data
+    finalLoss = 0.0
+    for testBatch in testGen:
+        if testBatch:
+            finalLoss += model.evaluate(testBatch[0], testBatch[1], verbose=0) / len(testBatch[1])
+        else:
+            break
+    print("Loss on test data:", finalLoss)
 
